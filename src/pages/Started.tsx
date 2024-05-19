af55816ab8a4729210b314da1ea838b63e1ceccd
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-05-19 18:44:04 +0530

    started page UI

---
+++ src/pages/Started.tsx
@@ -0,0 +1,26 @@
+import { IonButton, IonContent, IonIcon, IonPage } from "@ionic/react";
+import "./Started.css";
+
+const Started: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <div className="topright"> </div>
+
+        <div>
+          <div className="heading">
+            <h2>comarade </h2>
+            <h2>community</h2>
+          </div>
+        </div>
+        <div className="bmbtn">
+          <IonButton shape="round" color={"dark"} expand="full" size="large">
+            Get started <IonIcon name="arrow-forward"></IonIcon>
+          </IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Started;
