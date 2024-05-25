d42fc0664fb40201e37dcb8c8631586f0cc9fbe4
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-05-25 11:25:39 +0530

    create acc page

--- src/pages/Createacc.tsx
+++ src/pages/Createacc.tsx
@@ -1,4 +1,13 @@
-import { IonContent, IonIcon, IonPage } from "@ionic/react";
+import {
+  IonContent,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonList,
+  IonPage,
+  IonSelect,
+  IonSelectOption,
+} from "@ionic/react";
 import "./Createacc.css";
 import { chevronBack } from "ionicons/icons";
 
@@ -12,6 +21,32 @@ const Createacc: React.FC = () => {
         <div className="title">
           <p>create</p>
           <p>account</p>
+        </div>
+        <div className="inptfd">
+          <IonItem>
+            <IonInput label="Name"></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput label="Email"></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput label="Password" type="password"></IonInput>
+          </IonItem>
+        </div>
+        <div className="inptselt">
+          <IonList>
+            <IonItem>
+              <IonSelect placeholder="user type" interface="popover">
+                <div slot="label">user type</div>
+                <IonSelectOption value="user">user</IonSelectOption>
+                <IonSelectOption value="voluenteer">voluenteer</IonSelectOption>
+                <IonSelectOption value="layer">layer</IonSelectOption>
+                <IonSelectOption value="police">police</IonSelectOption>
+                <IonSelectOption value="Ngo">Ngo</IonSelectOption>
+                <IonSelectOption value="Hospitals">Hospitals</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+          </IonList>
         </div>
       </IonContent>
     </IonPage>
